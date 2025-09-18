68e1e83692c60b93577b02136e99986caa3341f2
bitturana-ab <b2ra830@gmail.com>
2025-09-18 22:37:29 +0530

    add imgae from network uri || url

--- app/index.tsx
+++ app/index.tsx
@@ -27,6 +27,17 @@ export default function Index() {
         source={require("../assets/images/icon.png")}
         style={{ width: 250, height: 250 }}
       />
+      <Image
+        source={{
+          uri: "https://images.unsplash.com/photo-1626808642875-0aa545482dfb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+        }}
+        style={{ width: 400, height: 400 }}
+      />
+
+      <Image
+        source={{ uri: "https://reactjs.org/logo-og.png" }}
+        style={{ width: 300, height: 300 }}
+      />
 
       <Link href="/users/ab">user 1</Link>
       <Link href="/users/yourname">user you</Link>
