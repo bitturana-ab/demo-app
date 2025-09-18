87c2997d8585201979622a30ed0a76e02d680d5c
bitturana-ab <b2ra830@gmail.com>
2025-09-18 23:15:04 +0530

    scroll view not FlatList will use

--- app/index.tsx
+++ app/index.tsx
@@ -9,6 +9,7 @@ import {
   Button,
   TouchableHighlight,
 } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -34,27 +35,41 @@ export default function Index() {
         </Link>
       </TouchableHighlight>
 
-      <ImageBackground
-        source={{
-          uri: "https://images.unsplash.com/photo-1626808642875-0aa545482dfb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
-        }}
-        style={{ width: " 90%", height: " 80%" }}
-      >
-        <TouchableHighlight underlayColor="red" onLongPress={hide}>
-          <Image
-            source={require("../assets/images/icon.png")}
-            style={{ width: 250, height: 250 }}
-          />
-        </TouchableHighlight>
-        <Text>You can write here</Text>
-        <Image
+      <ScrollView>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          rem similique dolores quos ab? Neque mollitia aperiam aut? A tempora
+          minus maiores culpa ipsam laborum aut impedit itaque aperiam commodi?
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+          consectetur amet sint voluptatibus quod doloribus aliquid similique
+          dolor perferendis error iure optio ut dignissimos mollitia, architecto
+          eveniet iste quidem sapiente. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Dignissimos necessitatibus, autem ipsa veniam quis
+          modi hic mollitia ducimus maiores sequi, commodi vitae recusandae
+          velit laudantium asperiores fugit voluptates magni aspernatur! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo ad incidunt ut dolorum animi veritatis debitis, atque sed enim, facere placeat a eaque, omnis officia? Autem, nihil officiis! Ducimus, consequatur?
+        </Text>
+        <ImageBackground
           source={{
-            uri: "https://reactjs.org/logo-og.png",
-            // cache: "only-if-cached",
+            uri: "https://images.unsplash.com/photo-1626808642875-0aa545482dfb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
           }}
-          style={{ width: 300, height: 300 }}
-        />
-      </ImageBackground>
+          style={{ width: " 90%", height: " 80%" }}
+        >
+          <TouchableHighlight underlayColor="red" onLongPress={hide}>
+            <Image
+              source={require("../assets/images/icon.png")}
+              style={{ width: 250, height: 250 }}
+            />
+          </TouchableHighlight>
+          <Text>You can write here</Text>
+          <Image
+            source={{
+              uri: "https://reactjs.org/logo-og.png",
+              // cache: "only-if-cached",
+            }}
+            style={{ width: 300, height: 300 }}
+          />
+        </ImageBackground>
+      </ScrollView>
 
       <Link href="/users/ab">user 1</Link>
       <Link href="/users/yourname">user you</Link>
