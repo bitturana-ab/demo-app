57d732a20a42f4ebf51208aa5433f668bcef19d1
bitturana-ab <b2ra830@gmail.com>
2025-09-18 17:23:16 +0530

    drawer has started:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,40 +6,39 @@ import {
   Label,
   Badge,
 } from "expo-router/unstable-native-tabs";
+import { Drawer } from "expo-router/drawer";
 
-// export default function RootLayout() {
-//   return (
-//     <>
-//       {" "}
-//       <StatusBar style="light" />
-//       <Stack />
-//     </>
-//   );
-// }
-export default function TabLayout() {
+export default function RootLayout() {
   return (
     <>
-      <NativeTabs>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-        {/* <NativeTabs.Trigger name="settings">
-          <Label>Settings</Label>
-          <Icon sf="house.fill" drawable="custom_android_drawable" />
-        </NativeTabs.Trigger> */}
-        <NativeTabs.Trigger name="search" role="search">
-          <Label>Search</Label>
-        </NativeTabs.Trigger>
-        <NativeTabs.Trigger name="index">
-          <Label>Home</Label>
-          <Icon sf="house.fill" drawable="custom_android_drawable" />
-        </NativeTabs.Trigger>
-        <NativeTabs.Trigger name="account">
-          <Icon sf="gear" drawable="custom_account_drawable" />
-          <Badge>9+</Badge>
-          <Label>Account</Label>
-        </NativeTabs.Trigger>
-      </NativeTabs>
+      <Drawer />
     </>
   );
 }
+// export default function TabLayout() {
+//   return (
+//     <>
+//       <NativeTabs>
+//         <Stack>
+//           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+//         </Stack>
+//         {/* <NativeTabs.Trigger name="settings">
+//           <Label>Settings</Label>
+//           <Icon sf="house.fill" drawable="custom_android_drawable" />
+//         </NativeTabs.Trigger> */}
+//         <NativeTabs.Trigger name="search" role="search">
+//           <Label>Search</Label>
+//         </NativeTabs.Trigger>
+//         <NativeTabs.Trigger name="index">
+//           <Label>Home</Label>
+//           <Icon sf="house.fill" drawable="custom_android_drawable" />
+//         </NativeTabs.Trigger>
+//         <NativeTabs.Trigger name="account">
+//           <Icon sf="gear" drawable="custom_account_drawable" />
+//           <Badge>9+</Badge>
+//           <Label>Account</Label>
+//         </NativeTabs.Trigger>
+//       </NativeTabs>
+//     </>
+//   );
+// }
